b0992536a4da71affb83f942afea5935bca8f3ef
Angelo Carvalho <angelosuporteti@gmail.com>
2020-10-29 13:55:36 -0300

    Adicionar arquivos de projeto.

---
+++ HerancaVsInterface/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HerancaVsInterface
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
